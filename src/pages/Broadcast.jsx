a0382392ad676ac3e58ffc7c29cf6c2bd42e4e69
eunbee1998 <eun061213@gmail.com>
2025-06-16 20:39:36 +0900

    Update Broadcast.jsx

--- src/pages/Broadcast.jsx
+++ src/pages/Broadcast.jsx
@@ -1,29 +1,30 @@
 import Navbar from "../components/Navbar";
-
-
 import React from "react";
+
 const videos = [
-  { title: "25년 GGF 예선 1경기", id: "abcd1234" },
-  { title: "25년 GGF 예선 2경기", id: "efgh5678" },
+  { title: "25년 GGF 예선 Day1 - AA팀 vs BB팀", id: "abcd1234" },
+  { title: "25년 GGF 예선 Day2 - CC팀 vs DD팀", id: "efgh5678" },
 ];
 
 export default function Broadcast() {
-  return (<>
-<Navbar />
-    <div className="page">
-      <h2>🎮 중계 영상</h2>
-      {videos.map((video, i) => (
-        <div key={i}>
-          <h4>{video.title}</h4>
-          <iframe
-            width="100%"
-            height="400"
-            src={`https://www.youtube.com/embed/${video.id}`}
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </div>
-      ))}
-    </div>
+  return (
+    <>
+      <Navbar />
+      <div className="page">
+        <h2 id="ggf-2025">📺 25년 GGF 중계 영상</h2>
+        {videos.map((video, i) => (
+          <div key={i}>
+            <h4>{video.title}</h4>
+            <iframe
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${video.id}`}
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
