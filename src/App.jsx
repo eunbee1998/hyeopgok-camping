1c9287c9f3d6ab6cbe3c10d5c0cf065ba29bddfb
eunbee1998 <eun061213@gmail.com>
2025-06-18 00:32:37 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import DuoChampions from "./pages/DuoChampions";  // 경로 맞게 수정해주세요
+import DuoChampions from "./pages/DuoChampions"; // 경로 맞게 수정해주세요
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -18,9 +18,8 @@ export default function App() {
         <Route path="/highlights" element={<Highlights />} />
         <Route path="/hall-of-fame" element={<HallOfFame />} />
         
-        {/* 여기에 아래 한 줄 추가 */}
+        {/* 여기에 DuoChampions 라우트 추가 */}
         <Route path="/hall-of-fame/duo" element={<DuoChampions />} />
-        
       </Routes>
     </Router>
   );
