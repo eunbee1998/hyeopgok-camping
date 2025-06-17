3d34d4c2cdb14473d7185306e15c6decbfd58e95
eunbee1998 <eun061213@gmail.com>
2025-06-18 00:30:25 +0900

    Create VideoCard.jsx

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function VideoCard({ video, onClick }) {
+  const thumbnailUrl = `https://img.youtube.com/vi/${video.id}/hqdefault.jpg`;
+
+  return (
+    <div className="video-card" onClick={() => onClick(video.id)} style={{ cursor: "pointer" }}>
+      <img src={thumbnailUrl} alt={video.title} style={{ width: "100%", borderRadius: "8px" }} />
+      <div style={{ padding: "8px" }}>
+        <h4 style={{ margin: "4px 0", fontSize: "1rem", color: "#fff" }}>{video.title}</h4>
+      </div>
+    </div>
+  );
+}
