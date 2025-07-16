c53a603b79040dae5ff9fc6b33aaafd1a9fd5526
eunbee1998 <eun061213@gmail.com>
2025-07-16 20:14:30 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import HighlightVideos from "./pages/HighlightVideos";
-import HallOfFame from "./pages/HallOfFame";
-import DuoChampions from "./pages/DuoChampions";
+import HallOfFame from "./pages/HallOfFame";              // 기존 명예의 전당 페이지
+import HighlightVideos from "./pages/HighlightVideos";    // 대회 하이라이트 페이지
+import DuoChampions from "./pages/DuoChampions";          // 협캠최강듀오 페이지
 import Navbar from "./components/Navbar";
 import "./styles.css";
 
@@ -13,11 +13,11 @@ export default function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/highlights" element={<HighlightVideos />} /> {/* 전체 대회 하이라이트 */}
-        <Route path="/highlights/:tournamentId" element={<HighlightVideos />} /> {/* 개별 대회 하이라이트 */}
-        <Route path="/hall-of-fame" element={<HallOfFame />} /> {/* 대회 연혁 */}
-        <Route path="/hall-of-fame/duo" element={<DuoChampions />} /> {/* 협캠최강듀오 */}
+        <Route path="/" element={<Home />} />                        {/* 홈 화면 */}
+        <Route path="/hall-of-fame" element={<HallOfFame />} />     {/* 대회 연혁 & 우승팀 */}
+        <Route path="/hall-of-fame/duo" element={<DuoChampions />} />{/* 협캠최강듀오 */}
+        <Route path="/highlights/highlightVideos" element={<HighlightVideos />} /> {/* 대회영상 > 대회 하이라이트 */}
+        <Route path="*" element={<HallOfFame />} />                  {/* 기본 페이지: 명예의 전당 */}
       </Routes>
     </Router>
   );
